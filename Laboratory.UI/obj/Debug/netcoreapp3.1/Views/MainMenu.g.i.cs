98d9ed69af44d03b77651b423d582461dc96626d
Abdul Hadi <Abdul.hadi.95@hotmail.com>
2021-07-12 14:28:00 -0700

    style editing

--- Laboratory.UI/obj/Debug/netcoreapp3.1/Views/MainMenu.g.i.cs
+++ Laboratory.UI/obj/Debug/netcoreapp3.1/Views/MainMenu.g.i.cs
@@ -114,13 +114,13 @@ namespace Laboratory.UI.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Laboratory.UI;component/views/mainmenu.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Laboratory.UI;V1.0.0.0;component/views/mainmenu.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainMenu.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -130,7 +130,7 @@ namespace Laboratory.UI.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
